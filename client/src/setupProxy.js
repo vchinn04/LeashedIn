f8d630505a605fa22069a588391c91d4ea97a6be
Dung Quang Ngo <ngoquangdung092003@gmail.com>
2022-12-02 00:27:52 -0800

    merging

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,8 +1,10 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
-    app.use(createProxyMiddleware('/api', {
-            target: "http://localhost:5000/",
+    app.use(
+        '/api', // You can pass in an array too eg. ['/api', '/another/path']
+        createProxyMiddleware({
+            target: 'http://localhost:5000',
             changeOrigin: true,
         })
     );
